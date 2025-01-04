0b549f6b5872833bdb5e67f87538fb8d61fb8087
Talhak <talhawkk@gmail.com>
2025-01-04 17:17:04 +0500

    adding field_order arguement to presenet all fields in an order

--- model/home/views.py
+++ model/home/views.py
@@ -4,5 +4,5 @@ from home.form import Admissions
 # Create your views here.
 def home(request):
     stud=Student.objects.all()
-    forms=Admissions(auto_id=True, label_suffix="->", initial={'name':'talhawk','email':'@gmail.com'})
+    forms=Admissions(auto_id=True, label_suffix="->", initial={'name':'talhawk','email':'@gmail.com'},field_order=['username','email','name','email'])
     return render(request,'home/home.html',{'stu':stud,'form':forms})
