16b19e4e2e7d6a8e8f2b12dde9a67f6b5a55646f
Talhak <talhawkk@gmail.com>
2025-01-04 16:56:05 +0500

    use of label_suffix

--- model/home/views.py
+++ model/home/views.py
@@ -4,5 +4,5 @@ from home.form import Admissions
 # Create your views here.
 def home(request):
     stud=Student.objects.all()
-    forms=Admissions(auto_id=True)
+    forms=Admissions(auto_id=True, label_suffix="->")
     return render(request,'home/home.html',{'stu':stud,'form':forms})
